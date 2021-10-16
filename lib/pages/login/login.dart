3247b74ba79306dec61f2c550897073ddbe87b8a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-16 15:52:24 -0500

    create log in form

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,3 +1,5 @@
+import 'package:bookclub/utils/theme.dart';
+import 'package:bookclub/widgets/app_card.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -16,12 +18,69 @@ class Login extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(40),
                   child: Image.asset("assets/logo.png"),
-                )
+                ),
+                const LoginForm(),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppCard(
+        child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+          child: Text(
+            "Login",
+            style: TextStyle(
+                color: Theme.of(context).secondaryHeaderColor,
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.alternate_email), hintText: "Email"),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.lock_outline), hintText: "Password"),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 100),
+            child: Text(
+              "Log in",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: const Text("Don't have an account? Sign up here"),
+          style: TextButton.styleFrom(
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap),
+        )
+      ],
+    ));
   }
 }
