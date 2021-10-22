0ca816641fd2e33c465c98928d4af54726896fd9
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-22 11:25:39 -0500

    feat: implement persistent session (#3)

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-  static const routeName = "/home";
+  static const routeName = "/";
 
   @override
   Widget build(BuildContext context) {
