22fcbb10a2dcf7576407648addc36c47eb4bbdb0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-22 18:01:51 -0500

    refactor(database): add void type to future

--- lib/providers/database.dart
+++ lib/providers/database.dart
@@ -5,7 +5,7 @@ import 'package:bookclub/utils/typedefs.dart';
 class Database {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  static Future createUser(User user) async {
+  static Future<void> createUser(User user) async {
     await _firestore.collection("users").doc(user.uid).set(user());
   }
 
