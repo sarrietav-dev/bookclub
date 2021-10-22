385178e5b1fa8a7a4a490d74d4adbb6d24c0b1cb
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-22 11:24:11 -0500

    feat: use button to sign out

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,7 @@
+import 'package:bookclub/pages/login/login.dart';
+import 'package:bookclub/providers/current_user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,6 +12,18 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("This is home"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            CurrentUser currentUser =
+                Provider.of<CurrentUser>(context, listen: false);
+            await currentUser.signOut();
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil(Login.routeName, (route) => false);
+          },
+          child: const Text("Sign Out"),
+        ),
       ),
     );
   }
