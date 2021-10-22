dba11fc16bd78ca9a4bfe23bba662c59e6336d48
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-22 17:56:02 -0500

    feat: implement get current user data from Firestore

--- lib/providers/database.dart
+++ lib/providers/database.dart
@@ -1,10 +1,22 @@
 import 'package:bookclub/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:bookclub/utils/typedefs.dart';
 
 class Database {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   static Future createUser(User user) async {
     await _firestore.collection("users").doc(user.uid).set(user());
+  }
+
+  static Future<User> getUser(String uid) async {
+    DocumentSnapshot<Json> snapshot =
+        await _firestore.collection("users").doc(uid).get();
+
+    return User(
+        uid: snapshot.id,
+        email: snapshot.get("email"),
+        name: snapshot.get("name"),
+        accountCreated: snapshot.get("accountCreated"));
   }
 }
